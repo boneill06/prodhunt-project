6190153511c472d2ffa7de5d40c327a155618cb2
Barry O'Neill <barryoneill06@gmail.com>
2018-10-28 10:37:08 +0000

    The final touches

--- product/views.py
+++ product/views.py
@@ -4,9 +4,10 @@ from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'product/home.html')
+    products = Product.objects
+    return render(request, 'product/home.html', {'product':products})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
@@ -32,3 +33,11 @@ def create(request):
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'product/detail.html',{'product':product})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/product/' + str(product.id))
